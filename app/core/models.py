6a871f269c592a83ae45f5fc4e560efb96b03bec
Goncalo Adolfo <g.adolfo@runtime-revolution.com>
2022-08-14 15:30:30 +0100

    Add user API

--- app/core/models.py
+++ app/core/models.py
@@ -9,8 +9,17 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('empty email field')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self.db)
         return user
 
